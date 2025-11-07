232db7dbe206094bb8ab8a78fe7984bc8e8adbe9
ruhul-amin-dev <125194470+ruhul-amin-dev@users.noreply.github.com>
2025-11-08 00:28:29 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-const yearEl = document.getElementById('year');
-yearEl.textContent = new Date().getFullYear();
-
-const btn = document.getElementById('cta');
-btn.addEventListener('click', () => {
-  alert('Welcome to Week 1 — deployed via GitHub Pages!');
-});
